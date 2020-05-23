049c1d15168c877aa55751d71427c7e5788e1873
Michael Jester <m.jester.93@gmail.com>
2020-05-23 04:16:56 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 def reverse_each_word(string)
+  array = []
+  final_string = string.split("")
+
   string.each do |x|
-    string.unshift()
+    array.unshift(x)
   end
 
+  final_string = array.
 end
